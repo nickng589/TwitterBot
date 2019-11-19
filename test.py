2fe1cf6f537376a2b1651a05517babeb60a5ae38
Nicholas Ng <nickng589@berkeley.edu>
2019-11-19 02:14:03 -0800

    first commit

---
+++ test.py
@@ -0,0 +1,15 @@
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+from keys import *
+
+youtube = googleapiclient.discovery.build('youtube','v3',developerKey=api_key)
+req = youtube.channels().list(part="statistics",id="UCOJplhB0wGQWv9OuRmMT-4g")
+res = req.execute()
+print(res.get("items")[0].get("statistics").get('viewCount'))
+request = youtube.videos().list(
+        part="statistics",
+        id="XEOCbFJjRw0"
+    )
+response = request.execute()
+print(response.get("items")[0].get("statistics").get('viewCount'))
