be7d86c57d1488580d92540d4514f416fa3ef3bb
Nicholas Ng <nickng589@berkeley.edu>
2021-01-02 04:16:02 -0800

    made it display images as well.  Also locally changed keys since the old twitter account doesnt work anymore:

--- bot.py
+++ bot.py
@@ -18,12 +18,15 @@ def main():
         twitter_AccessTokenSecret)
 
     api = tweepy.API(auth)
-    subs = getSubs(iZoneChannelID) + " subs for izone"
+
+    header = date + "\n"
+
+    subs = getSubs(iZoneChannelID) + " subs for izone(test3)"
     viewCountVioleta = getViews(violetaVideoID)
     violetaviews = u'\U0001F48E' + "Violeta views: " + viewCountVioleta
     lverviews = u'\U0001F339' + "La Vie en Rose views: " + getViews(lverID)
     print(subs + "\n" + violetaviews)
-    #api.update_status(subs + "\n" + violetaviews + "\n" + lverviews)
+    api.update_status(header + subs + "\n" + violetaviews + "\n" + lverviews)
     '''
     for status in api.user_timeline():
         print(status.id)
@@ -36,12 +39,15 @@ def main():
     ax.set_facecolor("#3C3C3C")
     fig.set_facecolor("#C3B0DE")
     createGraph(ax, data1, data2, {'color': '#B6E3BC'})
-    plt.show()
-
+    plt.savefig('test.png', facecolor="#C3B0DE")
+    #plt.show()
+    all_tweets = api.user_timeline()
+    media = api.media_upload("test.png")
+    print(media.media_id)
+    api.update_status(status="test pic", media_ids=[media.media_id], in_reply_to_status_id=all_tweets[0].id)
     data.close()
 
 #format data as date, time, numbers
-
 
 def writeData(file, tag, data):
     file.write("*"+tag + "\n")
